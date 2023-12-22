98f05890f50d501a16a5523254d234fd6240189c
Xenia Ch <eradonna@yandex.ru>
2023-12-22 20:23:28 +0300

    added response and error response

--- server.js
+++ server.js
@@ -8,12 +8,27 @@ const slow = require('koa-slow');
 const app = new Koa();
 const router = new Router();
 
+const news = [{
+  'date': '18.04 25.03.2019',
+  'header': '"Люди Икс: Темный феникс" - свой против своих. Показ стартует 7 июня'
+}, {
+  'date': '18.04 20.03.2019',
+  'header': '"Джон Уик 3" - продолжение истории наемного убийцы уже 16 мая в кино'
+}, {
+  'date': '18.05 19.03.2019',
+  'header': '"Мстители 4: Финал" показ стартует 25 апреля'
+}]
+
 router.get('/', async (ctx) => {
   ctx.response.body ='hello';
 });
 
 router.get('/news', async (ctx) => {
-  ctx.response.status = 500;
+  if (Math.random() > 0.5) {
+    ctx.response.body = JSON.stringify(news);;
+  } else {
+    ctx.status = 404;
+  }
 });
 
 app.use(koaBody());
